2e63a1445026db34b125f0cb78922c5f3121e927
Yakopoto <mfajarpamungkas@icloud.com>
2017-09-20 11:44:34 +0700

    trying

---
+++ array-of-objects.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var arr = []
+
+function createObj(name,phase,gender){
+    arr.push({name:name,phase:phase,gender:gender});
+}
+
+function getData(name){
+    for(let i = 0; i < arr.length;i++){
+        if(arr[i].name == name){
+            return arr[i];
+        }
+    }
+
+    return 'tidak ditemukan';
+}
+
+createObj('Akbar',1,'Male')
+createObj('Icha',1,'Female')
+createObj('Adhit',2,'Male')
+createObj('Tama',2,'Male')
+createObj('Rifky',3,'Male')
+
+console.log(arr);
+console.log(getData('Rifky'));
