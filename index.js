67e8453eb9a6728b89b5702fcaeb51160cbb0cb3
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-20 10:34:43 +0700

    Commit Release 1 array of objects

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ function createObj(name, phase, gender) {
   arr.push(obj);
 }
 
-createObj('Saptanto', 1, 'male');
-createObj('Sindu', 1, 'male');
+function getData(name) {
+  let result = {};
+  let indexResult = -1;
+  arr.forEach(function(curr, index) {
+    if (curr.name === name)
+      indexResult = index;
+  });
+  return (indexResult === -1) ? "Not Found" : arr[indexResult];
+}
+
+createObj('Akbar', 1, 'male');
+createObj('Icha', 1, 'female');
+createObj('Adhit', 1, 'male');
+createObj('Tama', 1, 'male');
+createObj('Rifky', 1, 'male');
 console.log(arr);
+console.log(getData("Sindu"));
+console.log(getData("Akbar"));
