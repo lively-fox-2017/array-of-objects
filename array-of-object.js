538b29fe70c9eb06f6f7861898ba2659ad9be911
wisnujaya <jayalahwisnu@gmail.com>
2017-09-20 11:55:08 +0700

    wisnudj

--- array-of-object.js
+++ array-of-object.js
@@ -10,10 +10,21 @@ function createObj(name, phase, gender) {
   arr.push(data)
 }
 
-createObj('icha', 1, 'female')
+createObj('Icha', 1, 'female')
 createObj('Akbar', 1, 'male')
 createObj('Adhit', 2, 'male')
 createObj('Tama', 2, 'male')
 createObj('Rifky', 3, 'male')
 
 console.log(arr);
+
+function getData(name) {
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i].name == name) {
+      return arr[i]
+    }
+  }
+  return -1
+}
+
+console.log(getData('Icha'));
