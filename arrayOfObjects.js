4f391c9b713ba7365c89da65e8217030d407a7ac
NNcrawler <rasyid.xyz@gmail.com>
2017-09-20 10:59:40 +0700

    release 1 Done with global var as processed data

--- arrayOfObjects.js
+++ arrayOfObjects.js
@@ -9,6 +9,15 @@ function createObj(name, phase, gender){
   return result;
 }
 
+function getData(name){
+  for(let i=0; i<arr.length; i++){
+    if(arr[i].name==name){
+      return arr[i];
+    }
+  }
+  return {};
+}
+
 arr.push(createObj('Akbar', 1, 'male'));
 arr.push(createObj('Icha', 1, 'female'));
 arr.push(createObj('Adhit', 2, 'male'));
@@ -16,3 +25,5 @@ arr.push(createObj('Tama', 2, 'male'));
 arr.push(createObj('Rifky', 3, 'male'));
 
 console.log(arr);
+console.log();
+console.log('Search data dari Icha: ',getData('Icha'));
